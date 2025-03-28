abfba537e4f07454e3d948379198994c1f3593cd
derek williams <derek61@gmail.com>
2025-03-28 16:03:10 -0600

    fixes to make example work

---
+++ example/env.example.com
@@ -0,0 +1,2 @@
+DB_CONNECTION=postgres://user:password@localhost:5432/gisthings
+DB_SCHEMA=public
--- example/src/graphile.config.ts
+++ example/src/graphile.config.ts
@@ -2,21 +2,14 @@ import 'graphile-config'
 import 'postgraphile'
 import { PostGraphileAmberPreset } from 'postgraphile/presets/amber'
 import { makePgService } from 'postgraphile/adaptors/pg'
-import { PgSimplifyInflectionPreset } from '@graphile/simplify-inflection'
-import { PgManyToManyPreset } from '@graphile-contrib/pg-many-to-many'
 
-//optional
-// import { PgPostgisWktPlugin } from './plugins/PgPostgisWktPlugin.js'
-import { PgPostgisPlugin } from './plugins/PgPostgisPlugin.js'
+import { PgPostgisWktPlugin } from 'postgraphile-postgis-wkt'
 
 const preset: GraphileConfig.Preset = {
   extends: [
     PostGraphileAmberPreset,
-    // PgSimplifyInflectionPreset,
-    // PgManyToManyPreset,
   ],
-  plugins: [PgPostgisPlugin], //Plugin is optional, remove if you don't need PostGIS support
-  // plugins: [PgPostgisWktPlugin], //Plugin is optional, remove if you don't need PostGIS support
+  plugins: [PgPostgisWktPlugin],
   pgServices: [
     makePgService({
       connectionString: process.env.DB_CONNECTION,
